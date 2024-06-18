1c4692b0eba5652537e63eb907cb91f2affb1466
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-06-18 17:07:08 +0900

    FEAT : stock 관련 패키지 생성

---
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -0,0 +1,29 @@
+package com.example.stock.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+public class Stock {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long productId;
+
+    private Long quantity;
+
+    public Stock() { }
+
+    public Stock(Long productId, Long quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+
+}
---
+++ src/main/java/com/example/stock/service/StockService.java
@@ -0,0 +1,13 @@
+package com.example.stock.service;
+
+
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StockService {
+
+    private final StockRepository stockRepository;
+}
