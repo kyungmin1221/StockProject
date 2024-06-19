8e7cf232e7669ba32539fc01f4e6941240827005
kyungmin <105621255+kyungmin1221@users.noreply.github.com>
2024-06-19 19:41:14 +0900

    FEAT : 재고 감소 로직 작성

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -5,7 +5,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -23,6 +22,13 @@ public class Stock {
     public Stock(Long productId, Long quantity) {
         this.productId = productId;
         this.quantity = quantity;
+    }
+
+    public void decrease(Long quantity) {
+        if(this.quantity - quantity < 0 ) {
+            throw new RuntimeException("재고는 0개 미만이 될 수 없습니다.");
+        }
+        this.quantity -= quantity;
     }
 
 
